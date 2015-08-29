d00a509d84e97f8cbf1b9d337dd63601b01b43ac
Andrew Keesler <andrew.keesler@silabs.com>
2015-08-28 23:57:33 -0400

    Util: make some simple calls inline.

--- src/util.c
+++ src/util.c
@@ -11,23 +11,23 @@
 #include "util.h"
 
 // Find the max of two values.
-double ANMAT_UtilMax(const double a,
-                     const double b)
+inline double ANMAT_UtilMax(const double a,
+                            const double b)
 {
   return (a > b ? a : b);
 }
 
 // Find the absolute value of a number.
-double ANMAT_UtilAbs(const double a)
+inline double ANMAT_UtilAbs(const double a)
 {
   return (a > 0 ? a : (-1 * a));
 }
 
 // Find out whether two floating point values are within a certain amount of
 // each other.
-bool ANMAT_UtilNeighborhood(const double a,
-                            const double b,
-                            const double epsilon)
+inline bool ANMAT_UtilNeighborhood(const double a,
+                                   const double b,
+                                   const double epsilon)
 {
   return (ANMAT_UtilAbs(a - b) < epsilon);
 }
